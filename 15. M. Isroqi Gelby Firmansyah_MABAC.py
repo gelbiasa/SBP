bc5f2d0299c7f63765b909bd27d5c4be7fc7fa16
M. Isroqi Gelby Firmansyah <117089848+gelbiasa@users.noreply.github.com>
2024-05-19 23:23:36 +0700

    Update 15. M. Isroqi Gelby Firmansyah_MABAC.py

--- 15. M. Isroqi Gelby Firmansyah_MABAC.py
+++ 15. M. Isroqi Gelby Firmansyah_MABAC.py
@@ -1,8 +1,10 @@
+# =============================================================================================================================================================================================
 # INFORMASI:
 
 # Sebelum melakukan Running saya mendownload tabulate terlebih dahulu  agar dapat melakukan running dengan tampilan tabel
 # Hal yang perlu dilakukan, jika menggunakan windows buka Command Prompt atau PowerShell, sedangkan jika menggunakan MacOS atau linux buka terminal.
 # Kemudian jalankan perintah "pip install tabulate"
+# ==============================================================================================================================================================================================
 
 from tabulate import tabulate
 
